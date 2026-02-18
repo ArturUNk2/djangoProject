3df37b5c14e22b67fbad5bd350c77ecedbff35cd
Artur <arturunkulov30@gmail.com>
2026-02-18 08:23:44 +0600

    update code ligin

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import registerview
+from .views import registerview,loginview
 urlpatterns = [
 
     path('register/', registerview),
+    path('login/', loginview),
 
 
 
--- users/views.py
+++ users/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegisterForm
 # Create your views here.
 
-def  registerview(reguest):
-    return render(reguest, 'register.html')
+
+def registerview(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("http://127.0.0.1:8000/next_to_page/")
+    else:
+        form = RegisterForm()
+
+    return render(request, "register.html", {"form": form})
+
+
+def loginview(request):
+    return render(request,"login.html")
